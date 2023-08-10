48562dda5d9408fb01caac4a6a9c79feecf3718f
portellii <101430995+portellii@users.noreply.github.com>
2023-08-09 22:57:43 -0700

    Added comments to HelloPyBullet.py

--- HelloPyBullet.py
+++ HelloPyBullet.py
@@ -5,20 +5,36 @@ import pybullet_data
 # p.DIRECT for non-graphical version
 physicsClient = p.connect(p.GUI)
 
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+# Get data path
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
+
+# Set gravity 
+p.setGravity(0,0,-9.8)
+
+# Load ground
 planeId = p.loadURDF('plane.urdf')
+
+# Init start position
 cubeStartPos = [0,0,1]
+
+# Init rotation
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+
+# Create ID for scripting 
 boxId = p.loadURDF('r2d2.urdf',cubeStartPos, cubeStartOrientation)
 
-for i in range (10):
+# Run sim for certain amount of time 
+for i in range (10000):
    p.stepSimulation()
    time.sleep(1./240.)
+
+# Get position and rotation
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print("GLEE")
+
+# Print the information
 print(cubePos,cubeOrn)
 
+# Disconnect from GUI
 p.disconnect()
 
 
